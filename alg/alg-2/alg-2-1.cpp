be19eb6083e244aab35070c3dbf84c5da2b15605
jingtao <851207685@qq.com>
2019-12-25 23:22:24 +0800

    使用map代替原来的set, 这样对value就可以随意的更改。但剪枝不完全导致产生循环，TODO 开销大且路径长的选项 不应该继续处理

--- alg/alg-2/alg-2-1.cpp
+++ alg/alg-2/alg-2-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <map>
 #include "string.h"
 #include <fstream>
 
@@ -65,7 +66,7 @@ class state
 		state():cost(0), dis(0), recalc(true){}
 		state(int cost, int dis):cost(cost), dis(dis), recalc(true){}
 		bool operator==(const state&); // 比较两个节点是相等的
-		size_t operator()(const state&) const; // 自定义类的哈希函数
+		int get_key(); // 计算hash属性
 		int cost;
 		int dis;
 		bool recalc;
@@ -73,60 +74,57 @@ class state
 bool state::operator==(const state& rhs){ 
 	return this->cost == rhs.cost && this->dis == rhs.dis;
 }
-size_t state::operator()(const state& rhs) const{
-	return size_t(rhs.cost << 16 + rhs.dis);
+int state::get_key(){
+	// operator '<<' has lower precedence than '+'
+	return (this->cost << 16) + this->dis;
 }
 
 int main(){
-	set<state *> s_test;
-	s_test.insert(new state());
-	(*s_test.begin())->recalc = false; // 存储的指针类型就可以实现这种操作
-
-
-
     vector<vector<int> > m1 = readMatrixFromTxt("m1.txt"); // 距离
 	vector<vector<int> > m2 = readMatrixFromTxt("m2.txt"); // 代价
 	const int Num_City = m1.size();
 	int i = 0;
-	vector<set<state> > dp(Num_City);
-	dp[0].insert(state()); // 初始化状态
+	vector<map<int,state*> > process(Num_City);
+	state *init = new state();
+	process[0][init->get_key()] = init; // 初始化状态
 
 	set<int> todos;
 	todos.insert(0); // 初始化，把自己放到需要处理的列表中
 
 	int p, d, c; // 处理的城市标号，距离，代价
 	set<int>::iterator it;
-	set<state>::iterator dp_it, dp_tmp;
+	map<int, state*>::iterator p_it, p_temp;
+	state *p_state;
 	while(!todos.empty()){
 		it = todos.begin(); // 集合中的第一个点
 		p = *it;
 		std::cout << "del " << p << "\t";
 		todos.erase(it); // 从集合中删除这个点
-		dp_it = dp[p].begin();
-		while(dp_it != dp[p].end()){
-			if(dp_it->recalc){
+
+		p_it = process[p].begin();
+		while(p_it != process[p].end()){
+			p_state = p_it->second;
+			if(p_state->recalc){
 				for(i = 0; i < Num_City; ++i){
 					d = m1[p][i], c = m2[p][i]; // p 到 i 的距离和代价
 					if(d == No_Way) continue; // 没有路的话，就不进行处理了
-					if(dp_it->cost + c > Max_Cost) continue; // 超过了最大的代价，剪枝
+					if(p_state->cost + c > Max_Cost) continue; // 超过了最大的代价，剪枝
+					// TODO 若存在路径又长，代价又高的情况，则也应该剪枝
 					// 判断节点是否已经存在
-					state s(dp_it->cost + c, dp_it->dis + d);
-					if((dp_tmp = dp[i].find(s)) != dp[i].end){ // 节点存在
-						if(dp_tmp->recalc == false){
-							dp[i].erase(dp_tmp);
-							dp[i].insert(s);
-						}
+					state *s = new state(p_state->cost + c, p_state->dis + d);
+					if((p_temp = process[i].find(s->get_key())) != process[i].end()){ // 节点存在
+						// p_temp->second->recalc = true; // 已经计算过了，没有继续
+						delete s;
 					}else{ // 没有找到
-						dp[i].insert(s);
+						process[i][s->get_key()] = s;
+						todos.insert(i); // 添加新的任务点
+						cout << "add " << i << "\t";
 					}
-					
-					todos.insert(i); // 添加新的任务点
-					cout << "add " << i << "\t";
 				}
 				// 更改recalc为false;
-				dp[p].erase(dp_it)// 蠢到家了，TODO，写不下去了
+				p_state->recalc = false;
 			}
-			++dp_it;
+			++p_it;
 		}
 		cout << endl;
 	}
