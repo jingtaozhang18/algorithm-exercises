ecea9fa0900a7da873680610e80c19a6ad39732b
jingtao <851207685@qq.com>
2020-04-03 16:19:36 +0800

    code-interview

---
+++ code-interview/1-reverseStack.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int getLastVal(stack<int>&mem){
+    int result=mem.top();
+    mem.pop();
+    if(mem.empty()) return result;
+    else {
+        int val=getLastVal(mem);
+        mem.push(result);
+        return val;
+    }
+}
+
+void reverse(stack<int>&mem){
+    if(!mem.empty()){
+        cout<<"enter"<<endl;
+        int val=getLastVal(mem);
+        cout << "Last: "<<val<<endl;
+        reverse(mem);
+        mem.push(val);
+    }
+}
+
+
+int main(void){
+    int N,val;
+    stack<int> mem;
+    cin>>N;
+    while(N--){
+        cin>>val;
+        mem.push(val);
+    }
+    reverse(mem);
+    
+    stack<int> rm;
+    while(!mem.empty()){
+        rm.push(mem.top());
+        mem.pop();
+    }
+    while(!rm.empty()){
+        cout<<rm.top()<<" ";
+        rm.pop();
+    }
+    cout<<endl;
+    return 0;
+}
