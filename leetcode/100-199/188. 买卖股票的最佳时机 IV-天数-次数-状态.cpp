29d2ce1e57a39d9cdfd3821136649567a37b0fb6
jingtao <851207685@qq.com>
2020-11-15 00:00:04 +0800

    代码积累

---
+++ leetcode/100-199/188. 买卖股票的最佳时机 IV-天数-次数-状态.cpp
@@ -0,0 +1,25 @@
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if(prices.size()<2) return 0;
+        int size=prices.size(),max_k=k,i,ans=0;
+        vector<vector<vector<int>>> dp(size, vector<vector<int>>(max_k+1, vector<int>(2,0)));
+        for(i=0;i<size;++i){
+            if(i==0) {
+                for(k=0;k<=max_k;++k) dp[i][k][1]=-prices[i];
+            }
+            else{
+                dp[i][0][1]=-prices[i];
+                for(k=max_k;k>=1;--k){
+                    dp[i][k][0]=max(dp[i-1][k][0], dp[i-1][k][1]+prices[i]); // 保持不持有或者卖出
+                    dp[i][k][1]=max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i]); // 保持持有或者买入
+                }
+            }
+        }
+        return dp[size-1][max_k][0];
+    }
+};
