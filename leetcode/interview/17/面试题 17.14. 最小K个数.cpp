b2c259d97e83064c2a223f7b381719dc98f73ecc
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-01-31 09:03:46 +0800

    代码积累

---
+++ leetcode/interview/17/面试题 17.14. 最小K个数.cpp
@@ -0,0 +1,88 @@
+// https://leetcode-cn.com/problems/smallest-k-lcci/
+#include<bits/stdc++.h>
+using namespace std;
+
+// 保持堆的大小不超过k
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+        vector<int> ans;
+        priority_queue<int, vector<int>, less<int>> que;
+        for(auto e:arr){
+            que.push(e);
+            if(que.size()>k) que.pop();
+        }
+        while(!que.empty()) {
+            ans.push_back(que.top());
+            que.pop();
+        }
+        return ans;
+    }
+};
+
+// 先建堆，取k个最小数字
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+        vector<int> ans;
+        priority_queue<int, vector<int>, greater<int>> que(arr.begin(), arr.end());
+        while(k--&&que.empty()==false) {ans.push_back(que.top()); que.pop();}
+        return ans;
+    }
+};
+
+// 原地堆排
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+        vector<int> ans;
+        int i, j, f, cl, cr, size=arr.size();
+        bool flag;
+        for(i=(size-1)>>1;i>=0;--i){ // 从下向上遍历所有的非叶子结点
+            j=i, flag=true; // 初始化指针
+            while(flag){ // 理顺该节点向下的大小关系
+                cl=(j<<1)+1, cr=(j<<1)+2, flag=false;
+                if(cl>=size);
+                else if(cr>=size){
+                    if(arr[cl]<arr[j]){
+                        swap(arr[cl], arr[j]);
+                        j=cl, flag=true;
+                    }
+                }else{
+                    if(arr[cr]<arr[cl]) cl=cr;
+                    if(arr[cl]<arr[j]){
+                        swap(arr[cl], arr[j]);
+                        j=cl, flag=true;
+                    }
+                }
+            }
+        }
+        while(k--){
+            ans.push_back(arr[0]);
+            swap(arr[0], arr[size-1]);
+            --size;
+            j=0, flag=true;
+            while(flag){
+                cl=(j<<1)+1, cr=(j<<1)+2, flag=false; // 初始化指针
+                if(cl>=size);
+                else if(cr>=size){
+                    if(arr[cl]<arr[j]){
+                        swap(arr[cl], arr[j]);
+                        j=cl, flag=true;
+                    }
+                }else{
+                    if(arr[cr]<arr[cl]) cl=cr;
+                    if(arr[cl]<arr[j]){
+                        swap(arr[cl], arr[j]);
+                        j=cl, flag=true;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// 保持堆大小不超过k       通过	   36 ms   18.3 MB	C++
+// 先建堆再取k个最小数字	通过	48 ms	18.7 MB C++
+// 原地堆排               通过	  40 ms	  18 MB   C++
