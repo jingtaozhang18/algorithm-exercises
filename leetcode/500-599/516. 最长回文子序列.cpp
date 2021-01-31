ad1db071c1e37bf1ca96e751dfb51e93fd682996
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-01-31 22:58:52 +0800

    代码积累

--- leetcode/500-599/516. 最长回文子序列.cpp
+++ leetcode/500-599/516. 最长回文子序列.cpp
@@ -6,7 +6,8 @@ class Solution {
 public:
     int longestPalindromeSubseq(string s) {
         int size=s.size(), i, j, diff;
-        vector<vector<int>>dp(size, vector<int>(size, 0));
+        // vector<vector<int>>dp(size, vector<int>(size, 0));
+        short dp[size][size]; // 优化
         for(diff=0;diff<size;++diff){
             for(i=0;i<size-diff;++i){
                 j=i+diff;
@@ -14,7 +15,8 @@ public:
                 else if(diff==1) dp[i][j]=s[i]==s[j]?2:1;
                 else{
                     if(s[i]==s[j]) dp[i][j]=dp[i+1][j-1]+2;
-                    dp[i][j]=max({dp[i+1][j], dp[i][j], dp[i][j-1]});
+                    // dp[i][j]=max({dp[i+1][j], dp[i][j], dp[i][j-1]});
+                    else dp[i][j]=max({dp[i+1][j], dp[i][j-1]}); // 优化
                 }
                 // cout<<dp[i][j]<<" ";
             }
