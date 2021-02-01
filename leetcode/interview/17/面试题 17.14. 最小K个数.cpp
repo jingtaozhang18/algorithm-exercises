cf70774db1c145249991ee2312b1dccdf396f4a3
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-02-01 18:55:08 +0800

    代码积累 by hp

--- leetcode/interview/17/面试题 17.14. 最小K个数.cpp
+++ leetcode/interview/17/面试题 17.14. 最小K个数.cpp
@@ -94,7 +94,24 @@ public:
     }
 };
 
+// heap 
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+        int i, size=arr.size();
+        make_heap(arr.begin(), arr.begin()+k, less<int>());
+        for(i=size-1;i>=k;--i){
+            push_heap(arr.begin(), arr.begin()+k+1, less<int>());
+            pop_heap(arr.begin(), arr.begin()+k+1, less<int>());
+            swap(arr[k], arr[i]);
+            arr.pop_back();
+        }
+        return arr;
+    }
+};
+
 // 保持堆大小不超过k       通过	   36 ms   18.3 MB	C++
 // 先建堆再取k个最小数字	通过	48 ms	18.7 MB C++
 // 原地堆排               通过	  40 ms	  18 MB   C++
-// make_heap            通过	40 ms	17.6 MB  C++ 
+// make_heap            通过	40 ms	17.6 MB  C++
+// make_small_heap      通过	28 ms	17.6 MB  C++
