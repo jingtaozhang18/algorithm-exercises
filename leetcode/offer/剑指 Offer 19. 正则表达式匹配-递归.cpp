4d44e2f7e2af3cd1f2bbd6cf6de23c5f6a5bbe97
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-03-09 21:45:01 +0800

    代码积累

--- leetcode/offer/剑指 Offer 19. 正则表达式匹配-递归.cpp
+++ leetcode/offer/剑指 Offer 19. 正则表达式匹配-递归.cpp
@@ -19,3 +19,29 @@ public:
         return match(s,p,0,0);
     }
 };
+
+// 复习 自底向上
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m=s.size(), n=p.size(), i, j;
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        for(j=n-1;j>0;j-=2){ // 处理边界
+            if(p[j]=='*') dp[m][j]=dp[m][j-1]=true; // p可以被截断
+            else break;
+        }
+        dp[m][n]=true;
+        for(j=n-1;j>=0;--j){ // p
+            for(i=m-1;i>=0;--i){ // s
+                if(p[j]!='*') dp[i][j]=dp[i][j]|((s[i]==p[j]||p[j]=='.')&dp[i+1][j+1]);
+                else{
+                    dp[i][j]=dp[i][j]||dp[i][j+1]; // 匹配0个
+                    if(s[i]==p[j-1]||p[j-1]=='.') dp[i][j]=dp[i][j]||dp[i+1][j]; // 匹配1+个  dp[i+1][j]匹配了0+个
+                    dp[i][j-1]=dp[i][j];
+                }
+            }
+            if(p[j]=='*') --j;
+        }
+        return dp[0][0];
+    }
+};
