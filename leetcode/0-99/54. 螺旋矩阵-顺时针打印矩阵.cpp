81ea739fb47e41ee267b0bf57832fc9cd35faa8b
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-03-15 10:01:26 +0800

    代码积累

--- leetcode/0-99/54. 螺旋矩阵-顺时针打印矩阵.cpp
+++ leetcode/0-99/54. 螺旋矩阵-顺时针打印矩阵.cpp
@@ -20,3 +20,29 @@ public:
         return ans;
     }
 };
+
+// 复习， 还是比较晕
+// 剥洋葱的做法，每次去掉一条边，去掉之前需要保证矩形还存在，相比于上面的方法要更好理解一些
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m=matrix.size(), n=m?matrix[0].size():0;
+        int up=0, down=m-1, left=0, right=n-1, i, j;
+        vector<int> ans;
+        while(up<=down&&left<=right){
+        // 拿掉上边，上边界往下缩一下
+        for(i=up,j=left;j<=right;++j)ans.push_back(matrix[i][j]);
+        up++;
+        // 若矩形还存在，拿掉右边，右边界往左缩一下
+        if(left<=right&&up<=down) for(i=up, j=right;i<=down;++i)ans.push_back(matrix[i][j]);
+        right--;
+        // 若矩形还存在，拿掉下边，下边界往上缩一下
+        if(up<=down&&left<=right) for(i=down,j=right;j>=left;--j)ans.push_back(matrix[i][j]);
+        down--;
+        // 若矩形还存在，拿掉左边，左边界往右缩一下
+        if(up<=down&&left<=right) for(i=down,j=left;i>=up;--i)ans.push_back(matrix[i][j]);
+        left++;
+        }
+        return ans;
+    }
+};
