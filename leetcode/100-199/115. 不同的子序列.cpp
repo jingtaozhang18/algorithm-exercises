acfdf6b0bda20737efe37c34306b5731c0558c06
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-03-17 09:40:03 +0800

    代码积累 by hp

---
+++ leetcode/100-199/115. 不同的子序列.cpp
@@ -0,0 +1,27 @@
+// https://leetcode-cn.com/problems/distinct-subsequences/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int i,j,m=s.size(), n=t.size();
+        long long dp[n+1][m+1];  // 错误点， 实现的时候，数组和思路写反了； 题目中保证答案不会越界， 不代表中间结果不会越界
+        // 初始化DP数组  dp[i][j]表示 s[0:i]子序列中为t[0:j]的数量
+        for(i=0;i<=n;++i){
+            for(j=0;j<=m;++j){
+                if(i==0) dp[i][j]=1;
+                else dp[i][j]=0;
+            }
+        }
+        for(i=1;i<=n;++i){
+            for(j=i;j<=m;++j){
+                if(t[i-1]==s[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                dp[i][j] += dp[i][j-1];
+            }
+        }
+        return dp[n][m];
+    }
+};
