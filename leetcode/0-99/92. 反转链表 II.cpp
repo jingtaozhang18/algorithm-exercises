412070a0248484f7c741ee217bd5aeb1673e5f55
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-03-18 07:39:59 +0800

    代码积累

--- leetcode/0-99/92. 反转链表 II.cpp
+++ leetcode/0-99/92. 反转链表 II.cpp
@@ -26,3 +26,25 @@ public:
         return head;
     }
 };
+
+// 复习，感觉思路没有第一次做的清晰
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        int i;
+        ListNode *a, *b, *c, *th=head, *tt; // th是前面不动的头，tt是动了的部分的尾巴
+        for(i=0;i<left-2&&th;++i) th=th->next;
+        if(i<left-2) return head;
+        a=nullptr, b=left>1?th->next:th, tt=nullptr; // 特殊处理整个反转的情况
+        for(i=0;i<right-left+1&&b;++i){ // 反转元素
+            c=b->next;
+            b->next=a;
+            a=b, b=c;
+            if(!tt) tt=a;
+        }
+        // a是翻转段最后一个，b是后面的头
+        if(left>1) th->next=a;
+        if(tt) tt->next=b;
+        return left>1?head:a;
+    }
+};
