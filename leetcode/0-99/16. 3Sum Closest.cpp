2aca2ae7bd6f17adf32e29e209be54479af4c268
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-04-09 08:47:46 +0800

    代码积累 by hp

--- leetcode/0-99/16. 3Sum Closest.cpp
+++ leetcode/0-99/16. 3Sum Closest.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// n^2*log(n)
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
@@ -27,6 +28,30 @@ public:
                 }
                 if(abs(x-target)<abs(ans-target)) ans=x;
             }
+        }
+        return ans;
+    }
+};
+
+// n^2
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int i, j, k, ans=-1, best=5*1e4, n=nums.size(), x;
+        for(i=0;i<n-2;){
+            j=i+1, k=n-1;
+            while(j<k){
+                x=nums[i]+nums[j]+nums[k];
+                if(abs(x-target)<best) best=abs(x-target), ans=x;
+                if(best==0) return ans;
+                else if(x<target){
+                    do{++j;}while(j<k&&nums[j-1]==nums[j]);
+                }else{
+                    do{--k;}while(j<k&&nums[k+1]==nums[k]);
+                }
+            }
+            do{++i;}while(i<n-2&&nums[i-1]==nums[i]);
         }
         return ans;
     }
