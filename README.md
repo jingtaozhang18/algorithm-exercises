9ccb60f43c1c97f821968741f1d65085a8b28b5d
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-04-23 20:46:06 +0800

    代码积累 by hp

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
   * [剑指offer-leetcode](leetcode/offer)
   * [程序员面试金典](leetcode/interview)
 * [面试实战](interview)
+* [微软 codility](codility)
 * [题目分类汇总](https://jingtao.fun/%E7%AE%97%E6%B3%95-%E9%A2%98%E7%9B%AE%E5%88%86%E7%B1%BB/)
 
 ## 头文件
---
+++ codility/BinaryGap.cpp
@@ -0,0 +1,21 @@
+// https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+#include<bits/stdc++.h>
+using namespace std;
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(int N) {
+    // write your code in C++14 (g++ 6.2.0)
+    int a=-1, b=-1, ans=0, i=0;
+    unsigned int flag=1;
+    while(N){
+        if(flag&N){
+            a=b, b=i;
+            if(a!=-1) ans=max(ans, b-a-1);
+        }
+        ++i, flag=flag<<1;
+        if(i==31) break;
+    }
+    return ans;
+}
