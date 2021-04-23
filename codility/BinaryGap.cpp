fc713a908540932d93f1b913bde4d107aca8bd7d
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-04-23 21:15:10 +0800

    代码积累 by hp

--- codility/BinaryGap.cpp
+++ codility/BinaryGap.cpp
@@ -19,3 +19,18 @@ int solution(int N) {
     }
     return ans;
 }
+
+int solution(int N) {
+    // write your code in C++14 (g++ 6.2.0)
+    int a=-1, b=-1, ans=0, i=0;
+    unsigned int flag=1;
+    while(N){
+        if(flag&N){
+            a=b, b=i;
+            if(a!=-1) ans=max(ans, b-a-1);
+        }
+        N=(~flag)&N;
+        ++i, flag=flag<<1; // 错误点，先去掉0，再移位，顺序反了
+    }
+    return ans;
+}
