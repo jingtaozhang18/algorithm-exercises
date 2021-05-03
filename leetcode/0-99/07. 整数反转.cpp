4fefd6bc98cbe8c3c542c5f46c769fd1a8605e84
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-05-03 10:05:35 +0800

    代码积累 by hp

--- leetcode/0-99/07. 整数反转.cpp
+++ leetcode/0-99/07. 整数反转.cpp
@@ -17,3 +17,23 @@ public:
       return ans;
     }
 };
+
+// 复习，惨不忍睹
+class Solution {
+public:
+    int reverse(int x) {
+      // 不要尝试对x乘以-1的操作，容易越界
+        int ans=0;
+        while(x){
+            if(ans>INT_MAX/10||ans<INT_MIN/10) return 0;
+            if(ans==INT_MAX/10&&x>INT_MAX-INT_MAX/10) return 0;
+            if(ans==INT_MIN/10&&x<INT_MIN-INT_MIN/10) return 0;
+            ans*=10, ans+=x%10, x/=10;
+        }
+        return ans;
+    }
+};
+
+// -2147483648
+// -2147483412
+// 1534236469
