0f47a20101594560edbf36b51059f168b101c03e
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-05-06 08:27:35 +0800

    代码积累 by hp

--- leetcode/1700-1799/1720 5649. 解码异或后的数组.cpp
+++ leetcode/1700-1799/1720 5649. 解码异或后的数组.cpp
@@ -15,3 +15,13 @@ public:
         return ans;
     }
 };
+
+// 复习
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> ans{first};
+        for(auto &e:encoded) ans.push_back(first^=e);
+        return ans;
+    }
+};
