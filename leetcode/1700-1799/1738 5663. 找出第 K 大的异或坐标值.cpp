ace7e55976654b81f3ef537aae815df259da53c9
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-05-19 23:32:33 +0800

    代码积累 by hp

--- leetcode/1700-1799/1738 5663. 找出第 K 大的异或坐标值.cpp
+++ leetcode/1700-1799/1738 5663. 找出第 K 大的异或坐标值.cpp
@@ -22,3 +22,25 @@ public:
         return que.top();
     }
 };
+
+// 每日一题
+class Solution {
+public:
+    int kthLargestValue(vector<vector<int>>& matrix, int k) {
+        int i, j, m=matrix.size(), n=matrix[0].size();
+        priority_queue<int, vector<int>, greater<int>> que;
+        for(i=1;i<m;++i){
+            for(j=0;j<n;++j){
+                matrix[i][j]^=matrix[i-1][j];
+            }
+        }
+        for(i=0;i<m;++i){
+            for(j=0;j<n;++j){
+                if(j>0) matrix[i][j]^=matrix[i][j-1];
+                if(que.size()<k||matrix[i][j]>que.top()) que.push(matrix[i][j]);
+                if(que.size()>k) que.pop();
+            }
+        }
+        return que.top();
+    }
+};
