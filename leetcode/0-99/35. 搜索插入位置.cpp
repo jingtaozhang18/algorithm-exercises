1d874f5b92ee95f9df6fbce952f2a7ad0d6529d1
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-05-23 23:36:24 +0800

    代码积累 by hp

---
+++ leetcode/0-99/35. 搜索插入位置.cpp
@@ -0,0 +1,17 @@
+// https://leetcode-cn.com/problems/search-insert-position/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0, right=nums.size()-1, mid;
+        while(left<=right){
+            mid=(left+right)>>1;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) left=mid+1;
+            else right=mid-1;
+        }
+        return left;
+    }
+};
