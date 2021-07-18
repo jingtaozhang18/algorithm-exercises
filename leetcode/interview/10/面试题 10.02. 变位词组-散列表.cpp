d165f62bccafa493ef1700e79bb40a7adfed05ac
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-07-18 20:44:33 +0800

    代码积累 by hp

--- leetcode/interview/10/面试题 10.02. 变位词组-散列表.cpp
+++ leetcode/interview/10/面试题 10.02. 变位词组-散列表.cpp
@@ -21,3 +21,19 @@ public:
       return ans;
     }
 };
+
+// 复习
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for(auto &str:strs) {
+            string t=str;
+            sort(t.begin(), t.end());
+            mp[t].push_back(str);
+        }
+        vector<vector<string>> ans;
+        for(auto &p:mp) ans.push_back(p.second);
+        return ans;
+    }
+};
