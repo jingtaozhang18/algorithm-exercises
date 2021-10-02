1642acaa08ba898245517d69f0cb9eeeb0a8826f
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-10-02 20:15:53 +0800

    代码积累 by hp

--- leetcode/剑指 Offer（第二版）/剑指 Offer 29. 顺时针打印矩阵.cpp
+++ leetcode/剑指 Offer（第二版）/剑指 Offer 29. 顺时针打印矩阵.cpp
@@ -1,22 +1,63 @@
 // https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        if(matrix.size()==0||matrix[0].size()==0) return ans;
-        int a=0,b=0,c=matrix.size()-1,d=matrix[0].size()-1,i,j;// (a, b)左上角， (c, d)右下角
-        while(a<=c&&b<=d){
-            i=a,j=b;
-            ans.push_back(matrix[i][j]);
-            if(b!=d) {for(++j;j<=d;++j) ans.push_back(matrix[i][j]); j--;}
-            if(a!=c) {for(++i;i<=c;++i) ans.push_back(matrix[i][j]); i--;}
-            if(a!=c) {for(--j;j>=b;--j) ans.push_back(matrix[i][j]); ++j;}
-            if(b!=d) {for(--i;i>a;--i) ans.push_back(matrix[i][j]); ++i;}
-            a++,b++,c--,d--;
-        }
-        return ans;
+ public:
+  vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> ans;
+    if (matrix.size() == 0 || matrix[0].size() == 0) return ans;
+    int a = 0, b = 0, c = matrix.size() - 1, d = matrix[0].size() - 1, i,
+        j;  // (a, b)左上角， (c, d)右下角
+    while (a <= c && b <= d) {
+      i = a, j = b;
+      ans.push_back(matrix[i][j]);
+      if (b != d) {
+        for (++j; j <= d; ++j) ans.push_back(matrix[i][j]);
+        j--;
+      }
+      if (a != c) {
+        for (++i; i <= c; ++i) ans.push_back(matrix[i][j]);
+        i--;
+      }
+      if (a != c) {
+        for (--j; j >= b; --j) ans.push_back(matrix[i][j]);
+        ++j;
+      }
+      if (b != d) {
+        for (--i; i > a; --i) ans.push_back(matrix[i][j]);
+        ++i;
+      }
+      a++, b++, c--, d--;
     }
+    return ans;
+  }
+};
+
+// 复习
+class Solution_fx {
+ public:
+  vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    if (matrix.size() == 0 || matrix[0].size() == 0) return {};
+    vector<int> ans;
+    int up = 0, down = matrix.size() - 1, left = 0,
+        right = matrix[0].size() - 1, i, j;
+    while (up <= down && left <= right) {
+      for (i = up, j = left; j <= right; ++j) ans.push_back(matrix[i][j]);
+      ++up;
+      if (left <= right && up <= down) {
+        for (i = up, j = right; i <= down; ++i) ans.push_back(matrix[i][j]);
+      }
+      --right;
+      if (left <= right && up <= down) {
+        for (i = down, j = right; j >= left; --j) ans.push_back(matrix[i][j]);
+      }
+      --down;
+      if (left <= right && up <= down) {
+        for (i = down, j = left; i >= up; --i) ans.push_back(matrix[i][j]);
+      }
+      ++left;
+    }
+    return ans;
+  }
 };
--- leetcode/剑指 Offer（第二版）/剑指 Offer 40. 最小的k个数.cpp
+++ leetcode/剑指 Offer（第二版）/剑指 Offer 40. 最小的k个数.cpp
@@ -1,22 +1,46 @@
 // https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
-public:
-    vector<int> getLeastNumbers(vector<int>& arr, int k) {
-        vector<int> ans;
-        if(k==0) return ans;
-        int i;
-        priority_queue<int, vector<int>, less<int>> que;
-        for(i=0;i<arr.size();++i){
-            if(que.size()<k) que.push(arr[i]);
-            else if(que.top()>arr[i]){que.pop();que.push(arr[i]);}
-        }
-        while(!que.empty()){
-            ans.push_back(que.top());
-            que.pop();
-        }
-        return ans;
+ public:
+  vector<int> getLeastNumbers(vector<int>& arr, int k) {
+    vector<int> ans;
+    if (k == 0) return ans;
+    int i;
+    priority_queue<int, vector<int>, less<int>> que;
+    for (i = 0; i < arr.size(); ++i) {
+      if (que.size() < k)
+        que.push(arr[i]);
+      else if (que.top() > arr[i]) {
+        que.pop();
+        que.push(arr[i]);
+      }
     }
+    while (!que.empty()) {
+      ans.push_back(que.top());
+      que.pop();
+    }
+    return ans;
+  }
+};
+
+// 复习
+class Solution_fx {
+ public:
+  vector<int> getLeastNumbers(vector<int>& arr, int k) {
+    priority_queue<int, vector<int>> que;
+    for (auto& e : arr) {
+      que.push(e);
+      if (que.size() > k) {
+        que.pop();
+      }
+    }
+    vector<int> ans;
+    while (que.size()) {
+      ans.push_back(que.top());
+      que.pop();
+    }
+    return ans;
+  }
 };
