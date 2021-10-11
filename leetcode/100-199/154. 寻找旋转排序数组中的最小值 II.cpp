76637edde91122f9e27742030899eee71b29e372
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-10-11 16:45:25 +0800

    代码积累 by hp

--- leetcode/100-199/154. 寻找旋转排序数组中的最小值 II.cpp
+++ leetcode/100-199/154. 寻找旋转排序数组中的最小值 II.cpp
@@ -6,24 +6,24 @@ class Solution {
 public:
     int findMin(vector<int>& nums, int left, int right) { // [left, right]
         int mid;
-        if(left==right||nums[left]<nums[right]) return nums[left];
-        mid=left+(right-left)/2;
-        return min(findMin(nums, left, mid), findMin(nums, mid+1, right));
+        if (left == right || nums[left] < nums[right]) return nums[left];
+        mid = left + (right - left) / 2;
+        return min(findMin(nums, left, mid), findMin(nums, mid + 1, right));
     }
     int findMin(vector<int>& nums) {
-        return findMin(nums, 0, nums.size()-1);
+        return findMin(nums, 0, nums.size() - 1);
     }
 };
 
-class Solution {
+class Solution_fx_1 {
 public:
     int findMin(vector<int>& nums) {
-        int left=0, right=nums.size()-1, mid;
-        while(left<right){
-            mid=left+(right-left)/2;
-            if(nums[mid]>nums[right]) left=mid+1;
+        int left = 0, right = nums.size() - 1, mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1;
             else {
-                if(nums[mid]<nums[right]) right=mid;
+                if (nums[mid] < nums[right]) right = mid;
                 else --right;
             }
         }
@@ -32,18 +32,18 @@ public:
 };
 
 // 复习， 从左边考虑
-class Solution {
+class Solution_fx_2 {
 public:
     int findMin(vector<int>& nums) {
-        int left=0, n=nums.size(), right=n-1, mid, k;
-        while(left<=right){
-            if(nums[left]<nums[right]) return nums[left];
-            mid=left+(right-left)/2;
-            if(nums[left]<nums[mid]) left=mid+1;
-            else if(nums[left]>nums[mid]) right=mid;
-            else{
-                for(k=left+1;k<=right;++k) if(nums[k-1]>nums[k]) break;
-                return (k==right+1)?nums[left]:nums[k];
+        int left = 0, n = nums.size(), right = n - 1, mid, k;
+        while (left <= right) {
+            if (nums[left] < nums[right]) return nums[left];
+            mid = left + (right - left) / 2;
+            if (nums[left] < nums[mid]) left = mid + 1;
+            else if (nums[left] > nums[mid]) right = mid;
+            else {
+                for (k = left + 1;k <= right;++k) if (nums[k - 1] > nums[k]) break;
+                return (k == right + 1) ? nums[left] : nums[k];
             }
         }
         return -1;
