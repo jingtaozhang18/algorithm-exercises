ec1f2aba0e9eb556a441c8cec802ebae24416ae9
jingtaozhang18 <sy1906605@buaa.edu.cn>
2021-10-16 17:03:44 +0800

    代码积累 by hp

---
+++ leetcode/200-299/282. 给表达式添加运算符.cpp
@@ -0,0 +1,70 @@
+// https://leetcode-cn.com/problems/expression-add-operators/
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+  char op;
+  long long num;
+  bool isOp;
+  node(char op) :op(op), isOp(true) {};
+  node(long long num) :num(num), isOp(false) {};
+};
+
+class Solution {
+public:
+  vector<string> ans;
+  // 乘法的巧妙处理，记录之前的乘积情况
+  void calc(string& num, long long pre, long long mul, int index, int target, bool isStart, vector<node>& path) {
+    if (index == num.size()) {
+      if (pre == target && path.size()) {
+        string str;
+        for (auto& e : path) {
+          if (e.isOp) {
+            str += e.op;
+          }
+          else {
+            str += to_string(e.num);
+          }
+        }
+        ans.push_back(str);
+      }
+      return;
+    }
+    long long x = 0;
+    int i;
+    for (i = index;i < num.size() && (i == index || num[index] != '0');++i) {
+      x *= 10;
+      x += num[i] - '0';
+      if (isStart) {
+        path.push_back(node(x));
+        calc(num, x, x, i + 1, target, false, path);
+        path.pop_back();
+      }
+      else {
+        path.push_back(node('*'));
+        path.push_back(node(x));
+        calc(num, pre - mul + mul * x, mul * x, i + 1, target, false, path);
+        path.pop_back();
+        path.pop_back();
+
+        path.push_back(node('+'));
+        path.push_back(node(x));
+        calc(num, pre + x, x, i + 1, target, false, path);
+        path.pop_back();
+        path.pop_back();
+
+        path.push_back(node('-'));
+        path.push_back(node(x));
+        calc(num, pre - x, -x, i + 1, target, false, path);
+        path.pop_back();
+        path.pop_back();
+      }
+    }
+  }
+  vector<string> addOperators(string num, int target) {
+    vector<node> path;
+    ans.clear();
+    calc(num, 0, 0, 0, target, true, path);
+    return ans;
+  }
+};
